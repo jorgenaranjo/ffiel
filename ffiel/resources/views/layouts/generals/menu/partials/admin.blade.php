72abf162fe0de8d57fd630de450daddb1a2aca64
jorgenaranjo <jorge020890@gmail.com>
2016-07-30 00:04:06 -0500

    [IMP] Se agrega ruta al menu

--- ffiel/resources/views/layouts/generals/menu/partials/admin.blade.php
+++ ffiel/resources/views/layouts/generals/menu/partials/admin.blade.php
@@ -3,7 +3,7 @@
         <li><a href="{{ route('workshops.index') }}"><i class="material-icons left">camera</i> {{ trans('menus.workshops') }}</a></li>
         <li><a href="#!"><i class="material-icons left">record_voice_over</i> {{ trans('menus.conferences') }}</a></li>
         <li><a href="#!"><i class="material-icons left">payment</i> {{ trans('menus.payments') }}</a></li>
-        <li><a href="#!"><i class="medium material-icons left">supervisor_account</i> {{ trans('menus.users') }}</a></li>
+        <li><a href="{{ route('users.index') }}"><i class="medium material-icons left">supervisor_account</i> {{ trans('menus.users') }}</a></li>
 
         <li><a class="dropdown-button" href="#!" data-activates="dropdown1">
                 {{ Auth::user()->name }} <i class="material-icons right">arrow_drop_down</i></a></li>
@@ -19,7 +19,7 @@
         <li><a href="{{ route('workshops.index') }}"><i class="material-icons left">camera</i> {{ trans('menus.workshops') }}</a></li>
         <li><a href="#!"><i class="material-icons left">record_voice_over</i> {{ trans('menus.conferences') }}</a></li>
         <li><a href="#!"><i class="material-icons left">payment</i> {{ trans('menus.payments') }}</a></li>
-        <li><a href="#!"><i class="medium material-icons left">supervisor_account</i> {{ trans('menus.users') }}</a></li>
+        <li><a href="{{ route('users.index') }}"><i class="medium material-icons left">supervisor_account</i> {{ trans('menus.users') }}</a></li>
         <li><a href="{{ route('logout') }}"><i class="fa fa-btn fa-sign-out"></i>{{ trans('menus.logout') }}</a></li>
     
 </ul>
