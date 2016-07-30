d755817b2e05834b2a20bae64cfa90943615aeab
jorgenaranjo <jorge020890@gmail.com>
2016-07-29 23:50:23 -0500

    [ADD] Users module

--- ffiel/app/Http/routes.php
+++ ffiel/app/Http/routes.php
@@ -16,3 +16,5 @@ require __DIR__ . '/Routes/workshop.php';
 require __DIR__ . '/Routes/paypal.php';
 //require __DIR__ . '/Routes/conekta.php';
 require __DIR__ . '/Routes/stripe.php';
+require __DIR__ . '/Routes/user.php';
+
